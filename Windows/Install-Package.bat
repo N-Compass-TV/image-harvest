6b1d23e54a0c17b5fdf46298019f5f3d2ebb359c
Fratz Antigua <fratz.antigua-at-892444632159>
2024-04-26 15:40:09 +0800

    Added more files

---
+++ Windows/Install-Package.bat
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Dependencies
+
+pip3 install requests
+pip3 install tqdm
+pip3 install datetime
+pip3 install selenium
+pip3 install selenium.webdriver
+pip3 install webdriver_manager
+pip3 install selenium.common
+pip3 install datetime
